b59a76385e758a6cf44b37b241933d8472b315b8
Rebeca MorenoRodriguez <morenreb@classwork.engr.oregonstate.edu>
2025-11-06 14:23:37 -0800

    Save local DML.sql changes

--- DML.sql
+++ DML.sql
@@ -8,6 +8,24 @@ UPDATE Users
     WHERE userId = @userIdInput
 DELETE FROM Users WHERE userId = @userIdInput;
 
+-- Movies table
+SELECT movieId, title, genre, release_date, lead_actor, streaming_platform 
+FROM Movies;
+
+INSERT INTO Movies (title, genre, release_date, lead_actor, streaming_platform)
+VALUES (@titleInput, @genreInput, @release_date_Input, @lead_actor_Input, @streaming_platform_Input);
+
+UPDATE Movies
+SET title = @titleInput,
+    genre = @genreInput,
+    release_date = @release_date_Input,
+    lead_actor = @lead_actor_Input,
+    streaming_platform = @streaming_platform_Input
+WHERE movieId = @movieIdInput;
+
+DELETE FROM Movies
+WHERE movieId = @movieIdInput;
+
 --SavedMovies table
 SELECT 
     Users.name AS user_name,
@@ -23,3 +41,22 @@ UPDATE SavedMovies
     WHERE userId = @userIdInput 
     AND movieId = @movieIdInput
 DELETE FROM SavedMovies WHERE userId = @userIdInput AND movieId = @movieIdInput;
+
+-- WatchedMovies table
+SELECT 
+    Users.name AS user_name,
+    Movies.title AS movie_title,
+    WatchedMovies.watched_date
+FROM WatchedMovies
+INNER JOIN Users ON WatchedMovies.userId = Users.userId
+INNER JOIN Movies ON WatchedMovies.movieId = Movies.movieId;
+
+INSERT INTO WatchedMovies (userId, movieId, watched_date)
+VALUES (@userIdInput, @movieIdInput, @watched_date_Input);
+
+UPDATE WatchedMovies
+SET watched_date = @watched_date_Input
+WHERE userId = @userIdInput AND movieId = @movieIdInput;
+
+DELETE FROM WatchedMovies
+WHERE userId = @userIdInput AND movieId = @movieIdInput;

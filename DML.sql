eba66d7ab4871232d9240b524348b922b263825d
Prisha Velhal <velhalp@flip3.engr.oregonstate.edu>
2025-11-06 14:19:24 -0800

    prisha's updated DML

--- DML.sql
+++ DML.sql
@@ -1,12 +1,25 @@
 --Users table
-SELECT userId, name, email FROM Users;
+SELECT userId, name, email FROM Users
 INSERT INTO Users (name, email)
-VALUES (@nameInput, @emailInput);
-UPDATE Users SET name = @nameInput, email = @emailInput WHERE userId = @userIdInput;
+VALUES (@nameInput, @emailInput)
+UPDATE Users 
+    SET name = @nameInput 
+    AND email = @emailInput 
+    WHERE userId = @userIdInput
 DELETE FROM Users WHERE userId = @userIdInput;
 
 --SavedMovies table
-SELECT userId, movieId, watched_date FROM SavedMovies;
+SELECT 
+    Users.name AS user_name,
+    Movies.title AS movie_title,
+    SavedMovies.saved_date
+FROM SavedMovies
+INNER JOIN Users ON SavedMovies.userId = Users.userId
+INNER JOIN Movies ON SavedMovies.movieId = Movies.movieId
 INSERT INTO SavedMovies (userId, movieId, saved_date)
-VALUES (@userIdInput, @movieIdInput, CURRENT_DATE);
+VALUES (@userIdInput, @movieIdInput, @savedDateInput)
+UPDATE SavedMovies
+    SET saved_date = @savedDateInput
+    WHERE userId = @userIdInput 
+    AND movieId = @movieIdInput
 DELETE FROM SavedMovies WHERE userId = @userIdInput AND movieId = @movieIdInput;

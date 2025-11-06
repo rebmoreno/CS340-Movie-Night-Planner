ed1dc0f82e3cfef7a29937ab91a73f40462ce277
Rebeca MorenoRodriguez <morenreb@classwork.engr.oregonstate.edu>
2025-11-06 15:59:24 -0800

    Update app.py and templates for saved/watched movies

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
+from flask import Flask, render_template, request, redirect
+import os
 
+app = Flask(__name__)
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
+
+@app.route('/saved-movies')
+def saved_movies():
+    return render_template('saved_movies.html')
+
+@app.route('/watched-movies')
+def watched_movies():
+    return render_template('watched_movies.html')
+
+# Listener 
+if __name__ == '__main__':
+    app.run(port=64890, debug=True)
